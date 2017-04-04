1f47cf14c3585101a3b089ee3c5d0d48a9a4e36b
Peter Sparks <psparks1992@gmail.com>
2017-04-04 17:33:19 +0000

    adding music mode (hopefully) and fixing underscore

--- keyboards/planck/keymaps/peter/keymap.c
+++ keyboards/planck/keymaps/peter/keymap.c
@@ -3,9 +3,9 @@
 
 #include "planck.h"
 #include "action_layer.h"
-//#ifdef AUDIO_ENABLE
-  //#include "audio.h"
-//#endif
+#ifdef AUDIO_ENABLE
+  #include "audio.h"
+#endif
 #include "eeconfig.h"
 
 extern keymap_config_t keymap_config;
@@ -62,13 +62,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = { 
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    LALT(LCTL(KC_DEL)) },
   {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______},
+  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_UNDS, KC_NUBS, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLD, KC_VOLU, KC_RGHT}
 },
 
 
 [_NUM] = { 
-  { RESET   , KC_F1 , KC_F2  , KC_F3  , KC_F4  , KC_NO  , KC_NO, KC_INSERT, KC_NO   , KC_7  , KC_8 , KC_9  } ,
+  { RESET   , MU_ON , MU_OFF  , KC_F3  , KC_F4  , KC_NO  , KC_NO, KC_INSERT, KC_NO   , KC_7  , KC_8 , KC_9  } ,
   { KC_NO   , KC_F5 , KC_F6  , KC_F7  , KC_F8  , KC_NO  , KC_NO, KC_DOWN, KC_UP, KC_4  , KC_5 , KC_6  } ,
   { _______ , KC_F9 , KC_F10 , KC_F11 , KC_F12 , LCTL(KC_MINS)  , S(LCTL(KC_MINS)), KC_LEFT, KC_RIGHT, KC_1  , KC_2 , KC_3  } ,
   { _______ , KC_LCTL , KC_LGUI  , KC_LALT  , KC_NO  , KC_ENT , KC_ENT, KC_INSERT, KC_NO  , KC_0 , KC_DOT , KC_SLSH }
