34ab1c7c2a7f3f224c1b11df1a10c9128a1c65e9
Peter Sparks <psparks1992@gmail.com>
2017-04-04 17:45:55 +0000

    fixing massive mistake with underscore

--- keyboards/planck/keymaps/peter/keymap.c
+++ keyboards/planck/keymaps/peter/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_PAUSE , KC_F1 , KC_F2   , KC_F3 , KC_F4 , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN   , KC_RPRN , KC_DEL},
   { _______  , KC_F5 , KC_F6   , KC_F7 , KC_F8 , KC_HOME, KC_NO  , KC_UNDS, KC_EQL , KC_LCBR   , KC_RCBR , KC_BSLS },
   { _______  , KC_F9 , KC_F10  , KC_F11, KC_F12, KC_END , KC_NO  , KC_UNDS, KC_PLUS, KC_LBRC   , KC_RBRC , _______ },
-  { _______  , KC_NO , _______ , KC_NO , KC_NO , KC_NO  , KC_NO  , KC_NO  , KC_VOLD, KC_VOLU   , KC_VOLD , KC_RIGHT  }
+  { _______  , KC_NO , _______ , KC_NO , _______ , KC_NO  , KC_NO  , KC_NO  , KC_VOLD, KC_VOLU   , KC_VOLD , KC_RIGHT  }
 },
 
 
